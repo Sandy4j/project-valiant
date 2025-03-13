6161ddd0fb374164d69fccee3109767504fd671c
Valonekiil <149128324+Valonekiil@users.noreply.github.com>
2025-03-13 11:08:24 +0700

    dialogue_done

--- UI/Dialogue/dialogue_pop_up.gd
+++ UI/Dialogue/dialogue_pop_up.gd
@@ -20,9 +20,7 @@ var dialogue_stopper = 0
 var convo_index
 
 func _ready():
-	tween = get_tree().create_tween()
 	#Dialogue.rect_global_position = Vector2(-13,700)
-	tween.connect("finished",self.on_tween_finished)
 	Dialogue.visible = false
 
 func load_dialogue(index:int):
@@ -30,19 +28,22 @@ func load_dialogue(index:int):
 	match index:
 		1:
 			dialogue_conv = dialogue_to_me
-			Dialogue.text = dialogue_conv[dialogue_index]
 			
 		
 	
 	Dialogue.visible = true
 	Indicator.visible = false
 	if dialogue_index < dialogue_conv.size():
+		print("dialog ke " + str(dialogue_index))
 		dialogue_finished = false
 		Dialogue.text = dialogue_conv[dialogue_index]
 		Dialogue.visible_ratio = 0
-		tween.interpolate_value(Dialogue,Dialogue.visible_ratio, 0,1,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
-		tween.start()
+		tween = create_tween()
+		tween.connect("finished",self.on_tween_finished)
+		tween.tween_property(Dialogue, "visible_ratio", 1, 1).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+		tween.play()
 	else :
+		print("dialog ended")
 		Dialogue.visible = false
 		DialogueBox.dial_hide()
 		dialogue_index = -1
@@ -51,6 +52,7 @@ func load_dialogue(index:int):
 	dialogue_index += 1
 
 func on_tween_finished():
+	print("conv done")
 	dialogue_finished = true
 	Indicator.visible = true
 
